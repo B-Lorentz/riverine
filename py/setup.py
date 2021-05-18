f43927718e587e6948a9627d4755928ffedceaed
B-Lorentz <44694582+B-Lorentz@users.noreply.github.com>
2021-05-18 17:41:45 +0200

    Create setup.py

---
+++ py/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='riverine',
+      version='0.0',
+      description='River simulations',
+      author='Bódy Lőrinc',
+      author_email='bodylorinc@gmail.com',
+      packages=['utils'],
+     )
